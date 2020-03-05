397913e1f860a6a6dbded492808891d414a2ab12
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-03-05 17:47:03 +0100

    add a semicolon at line 4 for codacy in router/index.js

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import Contact from "../views/Contact"
+import Contact from "../views/Contact";
 Vue.use(VueRouter);
 
 const routes = [
